f14e94e0641aa83c04c2b9c171b7010774d1a7a4
ligaoyong <ligaoyong@123.com>
2019-08-29 22:28:45 +0800

    add

--- src/main/java/com/threadPool/ThreadAndRunable.java
+++ src/main/java/com/threadPool/ThreadAndRunable.java
@@ -3,10 +3,18 @@ package com.threadPool;
 import org.junit.Test;
 
 public class ThreadAndRunable {
-    public static void main(String[] args) {
-        Runnable runnable = ()-> System.out.println("thread : "+Thread.currentThread().getName());
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> {
+            System.out.println("thread : " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
         Thread thread = new Thread(runnable);
         thread.setName("aaaa");
-        thread.run();
+        thread.start();
+        thread.join(); //在当前线程(Main)中等待thread线程执行完成再执行
     }
 }
