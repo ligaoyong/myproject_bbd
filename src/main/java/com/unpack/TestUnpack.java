44316b530f7144da3c18ba3338f52d8a4e9a88c9
ligaoyong <ligaoyong@123.com>
2019-09-01 20:24:12 +0800

    Merge branch 'master' of https://github.com/ligaoyong/myproject_bbd
    # Conflicts:
    #	pom.xml

--- src/main/java/com/unpack/TestUnpack.java
+++ src/main/java/com/unpack/TestUnpack.java
@@ -43,7 +43,12 @@ public class TestUnpack {
     }
 
 
-    //面向字节流得拆包与粘包现象
+
+
+    /**
+     * 面向字节流得拆包与粘包现象及解决方案
+     * @throws IOException
+     */
     @Test
     public void testServer() throws IOException {
         ServerSocket serverSocket = new ServerSocket();
@@ -53,12 +58,14 @@ public class TestUnpack {
             InputStream inputStream = socket.getInputStream();
 
             //重点api：inputStream.read() 读取下一个字节的数据(是真的数据，以int的形式返回 范围在-1，255之间)：也就是说一次读取一个字节
-            //如果是-1 则表示没有数据可读 流已经到达终点
+            //如果是-1 则表示没有数据可读 流已经到达终点(流被关闭)，否则阻塞读取
             //如果不是-1，则表示读到的是数据 我们需要把int转为byte，因为byte才是真正的数据
             //所有的api都是调用这个api来实现的
 
             /**
              * 不加控制的读取 在数据很大的时候 会出现粘包 拆包的问题
+             * 因为tcp是数据流，当客户端连续发送的数据且大小不统一时，服务端无法区分数据的界限
+             * 具体看：tcp粘包、拆包原理
              */
             /*byte[] bytes = new byte[1350];
             while ((inputStream.read(bytes)) != -1){ //有数据可读
@@ -68,20 +75,27 @@ public class TestUnpack {
             }*/
 
             /**
-             * 带控制的读取 先读取前四个字节 这四个字节表示真实数据的长度
+             * 带控制的读取 先读取前四个字节 这四个字节表示真实数据的长度(报文头)
+             * 在往后读取指定长度的数据(报文体)
+             * 往复如此 则不会出现拆包、粘包的问题
+             *
+             * 注意：使用sockeInputStream读取一个流的数据时，流的开头会出现4个字节的脏数据，分别是{-84,-19,0,5}
+             *      需要把开头的4个字节的脏数据排除掉
              */
+            byte[] zang = new byte[4]; //不知道为什么 流的开头会多出来4个字节的脏数据
+            inputStream.read(zang, 0, 4); //把脏数据读出来
+
             byte[] length = new byte[4];
             while (inputStream.read(length) == 4){ // 读取前4个字节 这4个字节表示数据的长度
                 //byte[] 转化为长度int
                 int value = 0;
                 for(int i = 0; i < 4; i++) {
                     int shift= (3-i) * 8;
-                    value += (length[i]) << shift;
+                    value += (length[i] & 0xff) << shift;
                 }
-
                 //data存放真实的数据
                 byte[] data = new byte[value];
-                int len = 0;
+                int len;
                 if ((len = inputStream.read(data)) == value) { //读取到正确的数据
                     System.out.println("读取到 ： " + len + "字节");
                     System.out.println(new String(data));
@@ -119,9 +133,12 @@ public class TestUnpack {
         System.out.println("s2 : " + s2.length());
         System.out.println("s1 :" + s1.getBytes(StandardCharsets.UTF_8).length+"字节");
         System.out.println("s2 :" + s2.getBytes(StandardCharsets.UTF_8).length+"字节"); // 1354个字节
-        for(int i=0;i<1;i++){
-            int length = s2.length();
-            byte[] datas = s2.getBytes();
+        for(int i=0;i<100;i++){
+            String str = i % 2 ==0 ? s1 : s2; //交替发送s1 s2
+
+            byte[] datas = str.getBytes(); //真实数据
+            int length = datas.length; //真实数据的字节数
+
             byte[] bytes = new byte[4 + datas.length]; //声明数组 其中前4个byte用来存数据的长度 那么可表示4GB的数据
             //将长度存在前面的4个字节中
             bytes[0] = (byte) ((length >> 24) & 0xff);
