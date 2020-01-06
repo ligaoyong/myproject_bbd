5b5b1442fe26e1c9df34bc977dafe150eb427b7f
ligaoyong <ligaoyong@ligaoyong.com>
2020-01-06 17:25:16 +0800

    classLoader

--- src/main/java/com/jvm/DestroyClassLoader.java
+++ src/main/java/com/jvm/DestroyClassLoader.java
@@ -1,6 +1,7 @@
 package com.jvm;
 
 import java.sql.*;
+import java.util.Enumeration;
 
 /**
  * 破坏 类加载双亲委派机制 ：让父类加载器请求子类加载器去加载类
@@ -11,7 +12,18 @@ import java.sql.*;
 public class DestroyClassLoader {
     public static void main(String[] args) throws SQLException {
         // 详细文档 https://blog.csdn.net/sinat_34976604/article/details/86723663
-        Connection connection = DriverManager.getConnection("jdbc:mysql://10.10.9.18:3306/information_schema");
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        Driver driver;
+        while (drivers.hasMoreElements())
+        {
+            driver = drivers.nextElement();
+            System.out.println(driver.getClass() + "------" + driver.getClass().getClassLoader());
+        }
+        System.out.println(DriverManager.class.getClassLoader());
+        // null DriverManager由BootstrapClassLoader加载的 BootstrapClassLoader由c++实现  并不存在与java中 所以为null
+
+
+        /*Connection connection = DriverManager.getConnection("jdbc:mysql://10.10.9.18:3306/information_schema");
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from character_sets");
         while (resultSet.next()){
@@ -21,6 +33,6 @@ public class DestroyClassLoader {
         }
         resultSet.close();
         statement.close();
-        connection.close();
+        connection.close();*/
     }
 }
