e810c60615aed6e6447afcb0f6501660c325b877
ligaoyong <ligaoyong@ligaoyong.com>
2020-01-19 15:56:03 +0800

    add spring session redis

--- src/main/java/com/session/redis/LoginController.java
+++ src/main/java/com/session/redis/LoginController.java
@@ -21,8 +21,10 @@ public class LoginController {
     /**
      * spring session 也是采用cookie 格式为session=xxxxx
      *
+     * 默认情况下 访问服务的时候就会创建session保存到redis中
+     *
      * 默认情况下，也就是应用程序不介入的情况下：
-     * 同一个客户端只会记住上一次登录的用户
+     *      同一个客户端只会记住上一次访问的会话 在不删除sessionId的情况下  同一个客户端都用同一个sessionId
      * 问题：
      *      如果同一个用户在不同的客户端登录，会创建不同的session 这样不同的session就会不同步
      * 解决方案：
