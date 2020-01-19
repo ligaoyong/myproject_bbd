a68732ae64de7f0c837a41c8048aa0fcbd733744
ligaoyong <ligaoyong@ligaoyong.com>
2020-01-19 15:32:41 +0800

    add spring session redis

--- src/main/java/com/session/redis/LoginController.java
+++ src/main/java/com/session/redis/LoginController.java
@@ -3,7 +3,6 @@ package com.session.redis;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
