b7e6ce104a44ed8f2cce7f2cb9a1ab2c4baa00aa
ligaoyong <ligaoyong@ligaoyong.com>
2020-01-19 15:27:15 +0800

    add spring session redis

--- src/main/java/com/session/redis/LoginController.java
+++ src/main/java/com/session/redis/LoginController.java
@@ -19,6 +19,22 @@ import java.util.Enumeration;
 @RestController
 public class LoginController {
 
+    /**
+     * 默认情况下，也就是应用程序不介入的情况下：
+     * 同一个客户端只会记住上一次登录的用户
+     * 问题：
+     *      如果同一个用户在不同的客户端登录，会创建不同的session 这样不同的session就会不同步
+     * 解决方案：
+     *      1、同一个用户允许多个客户端同时在线
+     *          1、1 多个会话之间不用共享数据，那么不用管，使用默认即可
+     *          1、2 多个会话之间需要共享数据，那么需要记录账号的多个session
+     *      2、只允许一个客户端在线
+     *          登录时检查会话是否存在，存在则删除其他客户端的会话，创建新的会话
+     * @param request
+     * @param userName
+     * @return
+     */
+
     @GetMapping("/login/{userName}")
     public String login(HttpServletRequest request, @PathVariable String userName){
         HttpSession session = request.getSession();
@@ -44,6 +60,13 @@ public class LoginController {
             jsonObject.put(name, session.getAttribute(name));
         }
         return jsonObject.toJSONString();
+    }
+
+    @GetMapping("/loginOut/{userName}")
+    public String loginOut(HttpServletRequest request, @PathVariable String userName){
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "退出登录，销毁session";
     }
 
 }
