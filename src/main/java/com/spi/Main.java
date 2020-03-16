82fdee7aa0abb115e522d664dbc98219e72e6144
ligaoyong <ligaoyong@123.com>
2020-03-16 22:19:04 +0800

    add SPI机制

--- src/main/java/com/spi/Main.java
+++ src/main/java/com/spi/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         //加载所有MyInterface的实现类(通常是框架来进行，这样就能加载到用户实现的类)
         //但实现类要声明在类路径下的META-INF/services,且文件名称一定要是接口的全限定名，内容是实现类的全限定名
+        /**
+         * mysql的驱动加载正是这种SPI机制，详细可看mysql-connector-java.jar
+         */
         ServiceLoader<MyInterface> serviceLoader = ServiceLoader.load(MyInterface.class);
         Iterator<MyInterface> iterator = serviceLoader.iterator();
         while (iterator.hasNext()){
